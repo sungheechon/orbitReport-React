6ab6f69cf8548fa47fca28037ab2a1b7814836b9
sunghee chon <sungheechon@sunghees-imac.lan>
2023-12-15 14:16:54 -0500

    GA6: Orbit Report - Task 1 and 2

---
+++ src/components/Banner.jsx
@@ -0,0 +1,10 @@
+const Banner = () => {
+    
+    return (
+        <>
+        <header>Orbit Report</header>
+        <p>Click on the buttons to see the satellites in that orbit type</p>
+        </>
+    )
+}
+export default Banner;
