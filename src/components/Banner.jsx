c63dfd003228f2717babe7911d37e9cbe31b5451
sunghee chon <sungheechon@sunghees-imac.lan>
2023-12-18 09:48:15 -0500

    CH 31: Reat, GA6: edit gitignore

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,10 +5,10 @@ const Banner = () => {
     
     return (
         <>
+        <div className="bg-galaxy"></div>
         <header>Orbit Report</header>
         <p>Click on the buttons to see the satellites in that orbit type</p>
         </>
     )
 }
-
 export default Banner;
